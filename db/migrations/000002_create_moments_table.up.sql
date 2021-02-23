18a05c3cbbc60a44312939b1fae04327742f7a36
brumhard <fobytobi@googlemail.com>
2021-02-23 19:17:07 +0100

    Update migration to only create type if it does not exist

--- db/migrations/000002_create_moments_table.up.sql
+++ db/migrations/000002_create_moments_table.up.sql
@@ -1,4 +1,13 @@
-CREATE TYPE moment_type AS ENUM ('start','startBreak','stopBreak','end');
+DO
+$$
+    BEGIN
+        CREATE TYPE moment_type
+        AS ENUM ('start','startBreak','stopBreak','end');
+    EXCEPTION
+        WHEN duplicate_object THEN null;
+    END
+$$;
+
 CREATE TABLE IF NOT EXISTS moments
 (
     id      serial,
